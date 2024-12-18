5aa9f2ec92dc954df48839a4497be46e1a9a207b
Chrisluan <havelucas00@gmail.com>
2024-12-18 19:05:09 -0300

    teste 2

--- src/backend/dataConnection.cjs
+++ src/backend/dataConnection.cjs
@@ -18,26 +18,21 @@ app.listen(port, () => {
 
 async function main() {
   const dbcon = process.env.mongodb;
-  // Conexão com o cliente MongoDB
   const client = new MongoClient(dbcon);
-  try {
-    // Conecta ao MongoDB
-    await client.connect();
 
-    // Acessa o banco de dados "maia" e a coleção "produtos"
+  try {
+    await client.connect();
     const db = client.db("maia");
     const collection = db.collection("products");
-
-    // Busca todos os documentos da coleção "produtos"
     const produtos = await collection.find({}).toArray();
 
-    // Exibe os produtos no console
     produtos.forEach((produto) => console.log(produto.name));
   } catch (e) {
     console.error("Erro ao buscar produtos:", e);
   } finally {
-    // Fecha a conexão com o cliente
     await client.close();
   }
 }
+
 main();
+
