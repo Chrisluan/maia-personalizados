ab06e75af464077d4a008f383227bfd2fc93c153
Chrisluan <havelucas00@gmail.com>
2024-12-18 22:21:23 -0300

    Teste3

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,9 +6,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/teste', (req, res) => {
+app.get('/hello', (req, res) => {
+  console.log('Endpoint /hello hit!');
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
